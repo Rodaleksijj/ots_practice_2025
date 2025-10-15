554b21d15ecdd46eb7ff7b494637777a181af0a0
Rodaleksijj Petruchenya <tie.znajesh.kto.jaa@yandex.ru>
2025-10-15 11:10:23 +0400

    Init task 1

--- task_1.py
+++ task_1.py
@@ -1,1 +1,22 @@
+# Лабораторная №1: Первичная инициализация
+# Курс: Основы теории систем
+# Студент: [Петрученя Родалексий Владимирович]
 
+def get_system_info():
+    """
+    Эта функция должна вернуть словарь с информацией о вашей "системе".
+    """
+    # TODO: Заполните словарь вашими реальными данными
+    system_info = {
+        "student_name": "Петрученя Родалексий Владимирович",
+        "academic_group": "ВТАСбз-21",
+        "github_link": "https://github.com/Rodaleksijj"
+    }
+    return system_info
+
+# Вывод информации для проверки
+if __name__ == "__main__":
+    info = get_system_info()
+    print("Информация о системе:")
+    for key, value in info.items():
+        print(f"- {key}: {value}")
